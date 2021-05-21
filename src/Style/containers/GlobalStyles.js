6667081a4d6df54e3cbad58ccb4b6c9e983f8c25
ibrohim777775 <ibrohim777775@gmail.com>
2021-05-21 23:50:40 +0300

    second commit

--- src/Style/containers/GlobalStyles.js
+++
@@ -1,9 +0,0 @@
-import styled, { createGlobalStyle } from "styled-components";
-import '../../Fonts/TTNormsPro/stylesheet.css';
-
-export const GlobalStyles = createGlobalStyle.div`
-  font-family: 'TT Norms Pro';
-  margin: 0;
-  padding: 0;
-  
-`
--- src/Style/containers/StyledMainPresent.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledMain = styled.div`
-  margin-top: 60px;
-  
-`
